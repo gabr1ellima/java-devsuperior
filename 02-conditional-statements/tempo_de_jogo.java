6815bc7e2ffafdbf2d0bc8416ea30be737eea3fb
gabr1ellima <gabriellima0409@gmail.com>
2023-12-27 19:03:00 -0300

    add conditional statements exercises

---
+++ 02-conditional-statements/tempo_de_jogo.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class tempo_de_jogo {
+
+	public static void main(String[] args) {
+		
+		// correcao: https://github.com/acenelio/curso-algoritmos/blob/master/java/tempo_de_jogo.java
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int horaInicial, horaFinal, dia, duracao;
+		
+		
+		System.out.print("Hora inicial: ");
+		horaInicial = sc.nextInt();
+		
+		System.out.print("Hora final: ");
+		horaFinal = sc.nextInt();
+		
+		dia = 24;
+		
+		duracao = dia + horaFinal - horaInicial;
+		
+		if (horaInicial > horaFinal) {
+			System.out.printf("O JOGO DUROU %d HORA(S)", duracao);
+		} 
+		else if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+			System.out.printf("O JOGO DUROU %d HORA(S)", duracao);
+		}
+		else {
+			System.out.println("A duracao da partida precisa ser maior que 1hr");
+		}
+		
+		sc.close();
+	}
+
+}
